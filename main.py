adef636c62b2b10b47e7286053ba24a603515aa4
asic <148511131+jguo55@users.noreply.github.com>
2024-07-24 20:45:00 -0700

    represent midicsv as string

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import torch
 import numpy as np
 import pandas as pd
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
 from pathlib import Path
 import os
+
+import torch.nn as nn
+import torch.nn.functional as F
 
 def find_classes(directory: str):
     classes = sorted(entry.name for entry in os.scandir(directory) if entry.is_dir())
@@ -19,13 +22,27 @@ class pieceDataset(Dataset):
         self.paths = list(Path(targ_dir).glob("*/*.csv"))
         self.transform = transform
         self.classes, self.class_to_idx = find_classes(targ_dir)
+        #tokendict = {'note_on': 'n', 'note_off': 'n', 'set_tempo': 't', 'time_signature': 's'}
+
+
 
     def __len__(self):
         return len(self.paths)
     
     def load_piece(self, index):
         piece_path = self.paths[index]
-        return pd.read_csv(piece_path)
+        tokens = ""
+        df = pd.read_csv(piece_path)
+        print(df.iloc[619]['type'])
+        for i in range(len(df)):
+            l = df.iloc[i]
+            if l['type'] == 'note_off' or l['type'] == 'note_on':
+                tokens += f"{l['tick']}" + 'c' + f"{l['channel']}" + 'n' + f"{l['note']}" + 'v' + f"{l['velocity']}" + " "
+            elif l['type'] == 'set_tempo':
+                tokens += f"{l['tick']}" + 't' + f"{l['tempo']}" + " "
+            elif l['type'] == 'time_signature':
+                tokens += f"{l['tick']}" + 'u' + f"{l['numerator']}" + 'd' + f"{l['denominator']}" + " "
+        return tokens
     
     def __getitem__(self, index):
         piece = self.load_piece(index)
@@ -37,11 +54,25 @@ class pieceDataset(Dataset):
 
 data_path = Path("./composer-classifier")
 piece_path = data_path/"data"
-
 train_dir = piece_path/"train"
+test_dir = piece_path/"test"
 
 train_data = pieceDataset(train_dir, transform=None)
+test_data = pieceDataset(test_dir, transform=None)
 
-item, cidx = train_data.__getitem__(1200)
-print(item)
-print(cidx)
+trainloader = DataLoader(dataset="train_data", batch_size=32, num_workers=0,shuffle=True)
+testloader = DataLoader(dataset="train_data", batch_size=32, num_workers=0,shuffle=True)
+
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+
+print(train_data.__getitem__(0)[0])
