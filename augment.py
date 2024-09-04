35bfb3726b6a7930a01a6658bc013be82b700af5
jguo55 <148511131+jguo55@users.noreply.github.com>
2024-09-03 17:52:52 -0700

    pre augment

--- augment.py
+++ augment.py
@@ -14,31 +14,35 @@ paths = list(Path(piece_path).glob("*/*.csv"))
 stretch = 1
 pitch = 4 #+/- pitch
 
+
 for k in range(len(paths)):
     filename = os.path.basename(paths[k])
-    dirname = os.path.basename(os.path.dirname(paths[k]))
-    path = str(paths[k])
-    df = pd.read_csv(paths[k])
-    notes = []
-    steps = []
-    durations = []
-    channels = []
-    velocities = []
+    if filename[:4] == 'aug_':
+        print(f"skip (augment) {filename}")
+    else:
+        dirname = os.path.basename(os.path.dirname(paths[k]))
+        path = str(paths[k])
+        df = pd.read_csv(paths[k])
+        notes = []
+        steps = []
+        durations = []
+        channels = []
+        velocities = []
 
-    for i in range(len(df)):
-        l = df.iloc[i]
-        if i % 100 == 0:
-            print(f"{k}/{len(paths)} -- " + path + f" -- {i}/{len(df)} lines")
-        notes.append(l['note']+pitch)
-        steps.append(l['step']*stretch)
-        durations.append(l['duration']*stretch)
-        channels.append(l['channel'])
-        velocities.append(l['velocity'])
-    
-    filename = f"s{stretch}p{pitch}-{filename}"
+        for i in range(len(df)):
+            l = df.iloc[i]
+            if i % 100 == 0:
+                print(f"{k}/{len(paths)} -- " + path + f" -- {i}/{len(df)} lines")
+            notes.append(l['note']+pitch)
+            steps.append(l['step']*stretch)
+            durations.append(l['duration']*stretch)
+            channels.append(l['channel'])
+            velocities.append(l['velocity'])
+        
+        filename = f"aug_s{stretch}p{pitch}-{filename}_"
 
-    dict = {'note': notes, 'step': steps, 'duration': durations, 'channel': channels, 'velocity': velocities}
-    output = pd.DataFrame(dict)
+        dict = {'note': notes, 'step': steps, 'duration': durations, 'channel': channels, 'velocity': velocities}
+        output = pd.DataFrame(dict)
 
-    outpath = data_path/"augment"/dirname/filename
-    output.to_csv(outpath)
+        outpath = piece_path/dirname/filename
+        output.to_csv(outpath)
